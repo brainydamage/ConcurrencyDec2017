11046347b463b46affeeda638b597da79e4756a9
Uliana Andreeva <Uliana_Andreeva@epam.com>
2017-12-08 17:03:54 +0300

    rename Worker and UseWorker

--- ConcurrencyDec2017/src/lesson171205/UseWorker.java
+++ ConcurrencyDec2017/src/lesson171205/UseWorker.java
@@ -2,37 +2,37 @@ package lesson171205;
 
 import lesson171201.Utils;
 
-public class UseMyWorker {
+public class UseWorker {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("main started");
 
-        MyWorker myWorker = new MyWorker();
+        Worker worker = new Worker();
 
-        myWorker.execute(() -> {
+        worker.execute(() -> {
             Utils.pause(1000);
             System.out.println("one");
         });
 
-        myWorker.execute(() -> {
+        worker.execute(() -> {
             Utils.pause(1000);
             System.out.println("two");
         });
 
-        myWorker.execute(() -> {
+        worker.execute(() -> {
             Utils.pause(1000);
             System.out.println("three");
         });
 
         //TODO подать какой-то сигнал, что здесь нужно завершить работу =>
-        //TODO MyWorker может завершать работу
-        myWorker.shutdown();
+        //TODO Worker может завершать работу
+        worker.shutdown();
 
-        myWorker.execute(() -> {
+        worker.execute(() -> {
             Utils.pause(1000);
             System.out.println("four");
         });
 
-        myWorker.getThread().join();
+        worker.getThread().join();
         System.out.println("main finished");
     }
 }
