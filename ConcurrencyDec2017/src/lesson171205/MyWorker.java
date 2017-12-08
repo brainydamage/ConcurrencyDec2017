ee12a1273268a472ab9b510c5c2fb22db774275f
Uliana Andreeva <Uliana_Andreeva@epam.com>
2017-12-08 17:04:37 +0300

    add MyWorker and UseMyWorker

---
+++ ConcurrencyDec2017/src/lesson171205/MyWorker.java
@@ -0,0 +1,70 @@
+package lesson171205;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Executor;
+
+public class MyWorker implements Executor {
+
+    private Thread thread;
+    private final Object mutex = new Object();
+    private Queue<Runnable> tasks = new LinkedList<>();
+    private boolean POISON_PILL;
+
+    public MyWorker() {
+        thread = new Thread(this::processTasks);
+        thread.start();
+    }
+
+    public Thread getThread() {
+        return thread;
+    }
+
+    public void execute(Runnable task) {
+
+        //добавление задачи в список задач
+        synchronized (mutex) {
+            //здесь несколько потоков могут пытаться изменить состояние одного объекта - race condition!
+            //следовательно необходима критическая секция synchronized{}
+            if (!POISON_PILL) {
+                tasks.offer(task);
+                //подаем сигнал о новой задаче тому потоку, который будет их обрабатывать
+                mutex.notify();
+            }
+        }
+    }
+
+    public void shutdown() {
+        synchronized (mutex) {
+            POISON_PILL = true;
+        }
+    }
+
+    //ссылку на этот метод будем использовать в качестве Runnable для запуска потока,
+    //так как он ничего не принимает и не возвращает
+    //
+    private void processTasks() {
+        //TODO сделать правильное завершение работы потока (shutdown, poison pill...) - DONE!
+        while (true) {
+            Runnable task = null;
+            //нужен блок synchronized над tasks.isEmpty() из-за проблемы visibility -
+            //изменения в tasks, внесенные другими потоками, не будут видны этого без критической секции
+            synchronized (mutex) {
+                //TODO why while, instead of if? read spurious wakeup
+                while (tasks.isEmpty()) {
+                    if (POISON_PILL) {
+                        return;
+                    }
+                    try {
+                        //не забывать о критическом блоке вокруг wait()
+                        mutex.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                task = tasks.poll();
+            }
+            task.run();
+        }
+    }
+}
---
+++ ConcurrencyDec2017/src/lesson171205/UseMyWorker.java
@@ -0,0 +1,38 @@
+package lesson171205;
+
+import lesson171201.Utils;
+
+public class UseMyWorker {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("main started");
+
+        Worker worker = new Worker();
+
+        worker.execute(() -> {
+            Utils.pause(1000);
+            System.out.println("one");
+        });
+
+        worker.execute(() -> {
+            Utils.pause(1000);
+            System.out.println("two");
+        });
+
+        worker.execute(() -> {
+            Utils.pause(1000);
+            System.out.println("three");
+        });
+
+        //TODO подать какой-то сигнал, что здесь нужно завершить работу =>
+        //TODO Worker может завершать работу
+        worker.shutdown();
+
+        worker.execute(() -> {
+            Utils.pause(1000);
+            System.out.println("four");
+        });
+
+        worker.getThread().join();
+        System.out.println("main finished");
+    }
+}
